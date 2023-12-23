0925aa1b1063481a05fc7866665960e83d8a27db
Henintsoa <handriamahadimbypgmai.com>
2023-12-23 15:39:53 +0100

    pré étapes 44

---
+++ Groupe1/2_SQL/2_CC2_Groupe1_SQL_OPE_Insert.sql
@@ -0,0 +1,14 @@
+-- Insection Client
+INSERT INTO dsid_liv_ope.client VALUES (1, 1, "nom1", "prenom1");
+
+-- Insertion Commande
+INSERT INTO dsid_liv_ope.commande VALUES (1, 1, 1, 1, 1, "01/01/2024", 100.1);
+
+-- Insertion Preparation
+INSERT INTO dsid_liv_ope.preparation VALUES (1, 1, 1, 1, "01/01/2024", "01/01/2024");
+
+-- Insertion Livraison
+INSERT INTO dsid_liv_ope.livraison VALUES (1, 1, 1, 1, "01/01/2024", "01/01/2024", 2);
+
+-- Insertion Restaurant
+INSERT INTO dsid_liv_ope.restaurant VALUES (1, 1, "code1", "raison_sociale_1");
