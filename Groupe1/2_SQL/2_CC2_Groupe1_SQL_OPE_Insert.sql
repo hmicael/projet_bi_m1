9097493439bd36473cd4962b4d84ba199b54d4b1
Henintsoa <handriamahadimbypgmai.com>
2023-12-25 23:41:43 +0100

    pré étapes 46

--- Groupe1/2_SQL/2_CC2_Groupe1_SQL_OPE_Insert.sql
+++ Groupe1/2_SQL/2_CC2_Groupe1_SQL_OPE_Insert.sql
@@ -1,14 +1,14 @@
--- Insection Client
-INSERT INTO dsid_liv_ope.client VALUES (1, 1, "nom1", "prenom1");
+-- Insertion Client(id_client, id_adresse_client, nom_client, prenom_client)
+INSERT INTO dsid_liv_ope.client VALUES (dsid_liv_ope.seq_id_client.nextval, 1, 'nom1', 'prenom1');
 
--- Insertion Commande
-INSERT INTO dsid_liv_ope.commande VALUES (1, 1, 1, 1, 1, "01/01/2024", 100.1);
+-- Insertion Restaurant(id_restaurant, id_adresse_restaurant, code_restaurant, 'raison_sociale_restaurant')
+INSERT INTO dsid_liv_ope.restaurant VALUES (dsid_liv_ope.seq_id_restaurant.nextval, 1, 'code1', 'raison_sociale_1');
 
--- Insertion Preparation
-INSERT INTO dsid_liv_ope.preparation VALUES (1, 1, 1, 1, "01/01/2024", "01/01/2024");
+-- Insertion Commande(id_commande, id_client, id_menu, id_moyen_paiement, numero_commande, date_commande, montant_total)
+INSERT INTO dsid_liv_ope.commande VALUES (dsid_liv_ope.seq_id_commande.nextval, 1, 1, 1, 1, to_date('01/01/2024','DD/MM/YYYY'), 100.1);
 
--- Insertion Livraison
-INSERT INTO dsid_liv_ope.livraison VALUES (1, 1, 1, 1, "01/01/2024", "01/01/2024", 2);
+-- Insertion Preparation(id_preparation, id_commande, id_restaurant, date_debut, date_fin)
+INSERT INTO dsid_liv_ope.preparation VALUES (dsid_liv_ope.seq_id_preparation.nextval, 1, 1, 1, to_date('01/01/2024','DD/MM/YYYY'), to_date('01/01/2024','DD/MM/YYYY'));
 
--- Insertion Restaurant
-INSERT INTO dsid_liv_ope.restaurant VALUES (1, 1, "code1", "raison_sociale_1");
+-- Insertion Livraison(id_livraison, id_preparation, id_livreur, numero_livraison, date_debut, date_fin, nombre_article_livre)
+INSERT INTO dsid_liv_ope.livraison VALUES (dsid_liv_ope.seq_id_livraison.nextval, 1, 1, 1, to_date('01/01/2024','DD/MM/YYYY'), to_date('01/01/2024','DD/MM/YYYY'), 2);
