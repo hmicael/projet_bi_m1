7720463957a60915a40dbaabd68ce20952edfa9d
Henintsoa <handriamahadimbypgmai.com>
2023-12-26 18:28:49 +0100

    ajout données pour rejet codepostal:st remy et ville: versaille

--- Groupe1/2_SQL/2_CC2_Groupe1_SQL_OPE_Insert.sql
+++ Groupe1/2_SQL/2_CC2_Groupe1_SQL_OPE_Insert.sql
@@ -4,6 +4,7 @@ INSERT INTO dsid_liv_ope.client VALUES (dsid_liv_ope.seq_id_client.nextval, 3, '
 
 -- Insertion Restaurant(id_restaurant, id_adresse_restaurant, code_restaurant, 'raison_sociale_restaurant')
 INSERT INTO dsid_liv_ope.restaurant VALUES (dsid_liv_ope.seq_id_restaurant.nextval, 1, 'code1', 'raison_sociale_1');
+INSERT INTO dsid_liv_ope.restaurant VALUES (dsid_liv_ope.seq_id_restaurant.nextval, 5, 'code1', 'raison_sociale_1');
 
 -- Insertion Commande(id_commande, id_client, id_menu, id_moyen_paiement, numero_commande, date_commande, montant_total)
 INSERT INTO dsid_liv_ope.commande VALUES (dsid_liv_ope.seq_id_commande.nextval, 1, 1, 1, 1, to_date('01/01/2024','DD/MM/YYYY'), 100.1);
